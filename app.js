a8c0da6fe818cbfe61d6c807263ce92759163c85
alexander <alexander@softwarebyaj.com>
2014-03-05 16:56:11 -0600

    working on this

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
  * Module dependencies.
  */
  
-var express = require('express')
-  , jsdom = require('jsdom')
-  , request = require('request')
-  , url = require('url')
-  , http = require('http')
-  , path = require('path')
+var express = require('express'),
+   request = require('request'),
+   cheerio = require('cheerio'),
+   url = require('url'),
+   http = require('http'),
+   path = require('path')
 ;
  
 var app = express();
@@ -30,38 +30,6 @@ app.configure('development', function(){
  
 app.get('/nodetube', function(req, res){
   //Tell the request that we want to fetch youtube.com, send the results to a callback function
-  request({uri: 'http://www.theweathernetwork.com/14-day-weather-trend/canada/saskatchewan/regina'}, function(err, response, body){
-    var self = this;
-    self.items = new Array();//I feel like I want to save my results in an array
- 
-    //Just a basic error check
-    if(err && response.statusCode !== 200){console.log('Request error.');}
-    //Send the body param as the HTML code we will parse in jsdom
-    //also tell jsdom to attach jQuery in the scripts and loaded from jQuery.com
-    jsdom.env({
-      html: body,
-      scripts: ['https://code.jquery.com/jquery-1.11.0.js'],
-      done: function(err, window){
-        //Use jQuery just as in a regular HTML page
-        var $ = window.jQuery;
-        var $body = $('body');
-        var $videos = $body.find('.trends-table tr:not(:first)');
- 
-        $videos.each( function(i, item) {
-          
-          self.items[i] = {
-            date:   $(this).find('td.date').html(),
-            high:   $(this).find('td.high span').html(),
-            low:    $(this).find('td.low span').html(),
-            pop:    $(this).find('td.pop strong').html()
-          };
-        });
- 
-        console.log( self.items );
-        res.end( "Done." );
-      }
-    });
-  });
 });
  
 http.createServer(app).listen(app.get('port'), function(){
