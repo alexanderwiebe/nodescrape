bb0fa9d091be70cc358b3f83be13ab3bb4496c64
alexander <alexander@softwarebyaj.com>
2014-03-07 16:59:54 -0600

    asdf

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 var express = require('express'),
    request = require('request'),
    cheerio = require('cheerio'),
+   jsdom = require('jsdom');
    url = require('url'),
    http = require('http'),
    path = require('path')
@@ -29,7 +30,54 @@ app.configure('development', function(){
 });
  
 app.get('/nodetube', function(req, res){
-  var myHtml = ''
+  jsdom.env({
+  url: "http://weather.gc.ca/rss/city/sk-32_e.xml",
+  scripts: ["http://code.jquery.com/jquery.js"],
+  done: function (errors, window) {
+    var $ = window.$;
+console.log('* * * * * * * * * *');
+var count = 0;
+var date = new Date();
+$("entry").slice(2).each(function(){
+var text = $(this).find('summary').html();
+text = text.replace(/minus /gi, '-');
+text = text.replace(/plus /gi, '+');
+
+console.log('*');
+console.log('high: ' + text.match(/high .*?\d+/gi));
+console.log('low: ' + text.match(/(low )|(wind chill ).*?\d+/gi));
+var day = $(this).find('title').html().match(/.*?:/gi)
+console.log('date: ' + now() + ' plus ' + count + ' days');
+console.log('info: ' + text);
+count++;
+});
+  }
+  });
+
+  jsdom.env({
+  url: "http://news.ycombinator.com/",
+  scripts: ["http://code.jquery.com/jquery.js"],
+  done: function (errors, window) {
+    var $ = window.$;
+    console.log("HN Links");
+    $("td.title:not(:last) a").each(function() {
+      console.log(" -", $(this).text());
+    });
+  }
+  });
+  /*
+        jsdom.env({
+          url: "http://www.theweathernetwork.com/14-day-weather-trend/canada/saskatchewan/regina",
+          scripts: ["http://code.jquery.com/jquery.js"],
+          done: function (errors, window) {
+            var $ = window.$;
+            console.log("HN Links");
+            $("td.title:not(:last) a").each(function() {
+            console.log(" -", $(this).text());
+          });
+        }
+        });
+
   request('http://www.theweathernetwork.com/14-day-weather-trend/canada/saskatchewan/regina', 
     function (error, response, html) { 
       if (!error && response.statusCode == 200) { 
@@ -47,10 +95,10 @@ app.get('/nodetube', function(req, res){
           }
         });
         res.send('worked');
-      } 
+      }*/ 
     }
   );
-});
+
  
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
